aa6055ecc4da3c7e82f10ebbeb17173e545150f2
github username <github email address>
2016-11-15 14:44:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 //Code React element here
+var meInReact = React.createElement('div', {className: "me"},
+  React.createElement('h1', {}, "An Awesome Person"),
+  React.createElement('p', {}, "Who is learning React"),
+  React.createElement('ul', {className: "me_interests"},
+  [
+    React.createElement('li', {key: "int1"}, "JavaScript"),
+    React.createElement('li', {key: "int2"}, "React"),
+    React.createElement('li', {key: "int3"}, "Movies"),
+    React.createElement('li', {key: "int4"}, "Ice cream")
+  ])
+);
+
+
+
+ReactDOM.render(
+  meInReact,
+  document.getElementById('main')
+);
